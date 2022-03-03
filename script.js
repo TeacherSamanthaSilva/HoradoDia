cf96ee7cd9c1cf433c952f7751a0785a2d22aa8d
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-03-03 01:10:55 -0300

    if
    Nesse commit terminei os ifs

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function carregar(){
+    
+    var mensagem = document.getElementById('mensagem')
+    var foto = document.getElementById('foto')
+    var data = new date()
+    var hora = data.gethours()
 
-    var mensagem = window.document.getElementById("mensagem");
-    var foto = document.getElementById("foto");
+    mensagem.innerHTML = "Agora são ${hora} horas"
 
+    if(hora >0 && hora>12){
+        foto.src = "images/morning.jpg"
+    }else if(hora <18 && hora > 12){
+        foto.src = "images/afternoon.jpg"
+        }else{
+            foto.src = "images/night.jog"
+        }
 }
