75321780325e1e5262b2f4a2877c04763b496fe7
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-03-03 01:17:53 -0300

    finalizando hora do dia
    Nesse commit finalizei o exercício hora do dia

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function carregar(){
     
     var mensagem = document.getElementById('mensagem')
-    var foto = document.getElementById('foto')
+    var imagem = document.getElementById('imagem')
     var data = new date()
     var hora = data.gethours()
 
     mensagem.innerHTML = "Agora são ${hora} horas"
 
     if(hora >0 && hora>12){
-        foto.src = "images/morning.jpg"
+        imagem.src = 'images/morning.jpg'
     }else if(hora <18 && hora > 12){
-        foto.src = "images/afternoon.jpg"
+        imagem.src = 'images/afternoon.jpg'
         }else{
-            foto.src = "images/night.jog"
+            imagem.src = 'images/night.jog'
         }
 }
